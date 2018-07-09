3e9c15aa9e6cc41a97ff37e30bfe037615787015
Julia Guillemin <julia.guillemingirls@gmail.com>
2018-07-09 20:22:07 +0000

    automatically backed up by learn

--- silly_string.rb
+++ silly_string.rb
@@ -2,7 +2,7 @@
 
 your_favorite_word = "Bubble"
 
-puts "My favorite word is #{your_favorite_word}"
+puts "My favorite word is #{your_favorite_word}."
 # Challenge 2: Capitalize every letter in your_favorite_word using a built in Ruby method
 
 favorite_word_in_all_caps = your_favorite_word.upcase # Put a . after your_favorite word and use a String method.
